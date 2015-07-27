ce9d8e11e3480111e10bff0963aa5ae607b3c1e6
Paul Grever <paulgrever@gmail.com>
2015-07-27 12:47:38 -0600

    added formating for phone numbers

--- app/models/prankee.rb
+++ app/models/prankee.rb
@@ -1,9 +1,25 @@
 class Prankee < ActiveRecord::Base
-  before_save :create_full_name
+  before_save :create_full_name, :format_number
 
   private
 
   def create_full_name
     self.full_name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def format_number
+    if self.phone_number[0..1] != "+1"
+      self.phone_number = "+1" + self.phone_number
+    end
+    if self.phone_number.include?("-" || "(" || ")" || "." || " ") 
+      phone = self.phone_number.tr("-", "")
+      phone = phone.tr(")", "")
+      phone =  phone.tr("(", "")
+      phone =  phone.tr(".", "")
+      phone =  phone.tr(" ", "")
+      self.phone_number = phone
+    else
+      self.phone_number
+    end
   end
 end
