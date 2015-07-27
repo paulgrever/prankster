421605b2bf4a6a21394f14aeaf642b585242e99d
Paul Grever <paulgrever@gmail.com>
2015-07-27 13:49:05 -0600

    added user and ability to login and logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726163316) do
+ActiveRecord::Schema.define(version: 20150727190057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20150726163316) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "full_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "user_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
